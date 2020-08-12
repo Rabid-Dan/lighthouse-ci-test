aa07e04ee3e5ff0a7b6d71d943f85eaf2bf79efb
Dan Morrison <danielroy.morrison@gmail.com>
2020-08-12 14:51:45 +1200

    install and run lighthouse ci tests locally

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,9 +2,24 @@ module.exports = {
   ci: {
     upload: {
       target: 'temporary-public-storage',
+      // basicAuth: {
+      //   username: "myusername",
+      //   password: "LighthouseRocks"
+      // },
     },
     assert: {
-      preset: 'lighthouse:recommended',
+      assertions: {
+          "categories:performance": ["error", {"minScore": 0.7}],
+          "categories:accessibility": ["error", {"minScore": 0.7}]
+      }
     },
+    collect: {
+      startServerCommand: "bundle exec rails s",
+      url: [
+        "http://localhost:3000/",
+        "http://localhost:3000/posts",
+        "http://localhost:3000/fruits"
+      ]
+    }
   },
 };
